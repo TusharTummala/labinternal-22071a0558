bcd6bdc2494d252931424143165c950da64ae6da
TusharTummala <153800476+TusharTummala@users.noreply.github.com>
2024-06-21 15:36:47 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => (
+  <footer>
+    <p>&copy; 2023 Tushar's website 22071A0558</p>
+  </footer>
+);
+
+export default Footer;
---
+++ home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./home.css";
+const Home = () => (
+  <div>
+    <h1>Welcome to our Zoop Site!</h1>
+    <p>Shop the best products at the best prices.</p>
+  </div>
+);
+
+export default Home;
